11d788fe8b38b6accee023a0fceda2b6ff1542b1
ArkoAnnuk <arkoannuk+prog@gmail.com>
2023-07-11 02:33:26 +0300

    Test

--- src/components/LeadGrid/LeadGrid.tsx
+++ src/components/LeadGrid/LeadGrid.tsx
@@ -9,16 +9,16 @@ export function LeadGrid() {
   return (
     <Container pb={0} pt={0}>
       <SimpleGrid cols={2} spacing="md" breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
-         <Image src='src/components/LeadGrid/result2.jpg' alt='Random image' height={PRIMARY_COL_HEIGHT} radius="md" />
+         <Image src='https://res.cloudinary.com/dgjfea9cb/image/upload/v1689031670/taretark/result2_ikv5qe.jpg' alt='Random image' height={PRIMARY_COL_HEIGHT} radius="md" />
         <Grid gutter="md">
           <Grid.Col>
-             <Image src='src/components/LeadGrid/result3.jpg' alt='Random image' height={SECONDARY_COL_HEIGHT} radius="md" />
+             <Image src='https://res.cloudinary.com/dgjfea9cb/image/upload/v1689031668/taretark/result3_lev1ge.jpg' alt='Random image' height={SECONDARY_COL_HEIGHT} radius="md" />
           </Grid.Col>
           <Grid.Col span={6}>
-             <Image src='src/components/LeadGrid/image4.jpg' alt='Random image' height={SECONDARY_COL_HEIGHT} radius="md" />
+             <Image src='https://res.cloudinary.com/dgjfea9cb/image/upload/v1689031828/taretark/result4_u80as7.jpg' alt='Random image' height={SECONDARY_COL_HEIGHT} radius="md" />
           </Grid.Col>
           <Grid.Col span={6}>
-             <Image src='src/components/LeadGrid/result1.jpg'  alt='Random image' height={SECONDARY_COL_HEIGHT} radius="md" />
+             <Image src='https://res.cloudinary.com/dgjfea9cb/image/upload/v1689031669/taretark/result1_kv4ayn.jpg'  alt='Random image' height={SECONDARY_COL_HEIGHT} radius="md" />
           </Grid.Col>
         </Grid>
       </SimpleGrid>
